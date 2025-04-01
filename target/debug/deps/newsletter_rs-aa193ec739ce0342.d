236acd650c712d740e2135da4c8010f4cb278c73
onelrian <onelsob57@gmail.com>
2025-04-01 16:39:58 +0100

    Chore: Removed target directory from Git tracking

--- target/debug/deps/newsletter_rs-aa193ec739ce0342.d
+++
@@ -1,5 +0,0 @@
-/home/sob/Projects/Rust-SDLC/newsletter-rs/target/debug/deps/libnewsletter_rs-aa193ec739ce0342.rmeta: src/main.rs
-
-/home/sob/Projects/Rust-SDLC/newsletter-rs/target/debug/deps/newsletter_rs-aa193ec739ce0342.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/newsletter_rs-ddb46a65fb2b7a9f.d
+++
@@ -1,5 +0,0 @@
-/home/sob/Projects/Rust-SDLC/newsletter-rs/target/debug/deps/libnewsletter_rs-ddb46a65fb2b7a9f.rmeta: src/lib.rs
-
-/home/sob/Projects/Rust-SDLC/newsletter-rs/target/debug/deps/newsletter_rs-ddb46a65fb2b7a9f.d: src/lib.rs
-
-src/lib.rs:
