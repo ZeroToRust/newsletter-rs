011ee1bc9c0abbad13b00b12b1ad63b833ce8a4a
jagoum <jagoum24@gmail.com>
2025-04-02 16:45:48 +0100

    chore: added unit test using tower and hyper to simulate request and the process of sending request

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,29 @@ async fn main() -> Result<(), Box<dyn Error>> {
 async fn health_check() -> String {
     "Health Check Successful".into()
 }
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use axum::{body::Body, http::{Request, StatusCode}};
+    use tower::ServiceExt; // for `oneshot` method
+
+    #[tokio::test]
+    async fn test_health_check() {
+        // Create the app with the route
+        let app = Router::new().route("/health-check", get(health_check));
+
+        // This Simulate a GET request to the `/health-check` route like a client
+        let request = Request::builder()
+            .uri("/health-check")
+            .method("GET")
+            .body(Body::default())
+            .unwrap();
+        // simulate the process of sending the request to the app like when a client navigate to the path on the browser
+        let response = app.oneshot(request).await.unwrap();
+
+        // Checking if the response was a successful one
+        assert_eq!(response.status(), StatusCode::OK);
+
+    }
+}
