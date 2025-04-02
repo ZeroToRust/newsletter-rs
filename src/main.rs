352ace57471873dbc4e207bd6b114fae6c1a177c
jagoum <jagoum24@gmail.com>
2025-04-02 15:32:12 +0100

    update: updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::error::Error;
 
-async fn response() -> impl Responder {
-    HttpResponse::Ok()
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new().route("/health-check", get(health_check));
+    let address = TcpListener::bind("0.0.0.0:8080").await?;
+    println!("Server serving on http://localhost:8080/health-check");
+    let _server = axum::serve(address, app).await?;
+    Ok(())
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let _ = HttpServer::new(|| App::new().route("/health_check", web::get().to(response)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await;
-
-    Ok(())
+async fn health_check() -> String {
+    "Health Check Successful".into()
 }
