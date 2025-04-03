48a3f91b31b4218966ca9a673a54edfec2fced4c
jagoum <jagoum24@gmail.com>
2025-04-03 13:19:06 +0100

    Update src/main.rs
    Co-authored-by: Hermann Core <65020150+Hermann-Core@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let app = Router::new().route("/health-check", get(health_check));
+    let app = Router::new().route("/health_check", get(health_check));
     let address = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Server serving on http://localhost:8080/health-check");
     let _server = axum::serve(address, app).await?;
