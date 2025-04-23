0ecd9ab7609619fb91559359e5a46f39109a6ddf
jagoum <jagoum24@gmail.com>
2025-04-23 17:23:23 +0100

    refactor(dbconnection): refactor connection configuration to reduce biolerplate

---
+++ src/handlers/db_util.rs
@@ -0,0 +1,12 @@
+use dotenvy::{self, dotenv, var};
+use sqlx::PgPool;
+pub async fn get_database_pool() -> PgPool {
+    // Load the DATABASE_URL from the environment
+    dotenv().ok(); // Loads env variables from the .env file
+    let durl = var("DATABASE_URL").unwrap(); // Gets the database url from the .env file
+                                             // Create and return the connection pool
+    let pool = PgPool::connect(&durl)
+        .await
+        .expect("Failed to connect to database");
+    pool
+}
