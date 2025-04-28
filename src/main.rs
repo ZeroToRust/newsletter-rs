585f532d0d6f1b2e5100724b24efe48b218628e2
onelrian <onelsob57@gmail.com>
2025-04-28 11:40:00 +0100

    style:Formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use axum::serve;
+use eyre::Result;
 use newsletter_rs::startup::build_app;
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // Build the application
     let app = build_app();
 
     // Run the application
-    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Server Running on http://0.0.0.0:8080");
-    serve(listener, app).await.unwrap();
+    serve(listener, app).await?;
+
+    Ok(())
 }
