ee1c997605bc3558ea5151466a84ad11a718eb34
donemmanuelo <emmanuelodon943@gmail.com>
2025-05-02 04:09:15 +0100

    integration test to test certain functionalities of the database using posgres container

---
+++ tests/intergration_test.rs
@@ -0,0 +1,45 @@
+use postgres::Postgres;
+use testcontainers_modules::{postgres, testcontainers::{core::{Mount, Image}, runners::AsyncRunner, ContainerRequest}};
+use tokio_postgres::{Client, NoTls};
+use std::{path::PathBuf, thread::sleep, time::Duration};
+
+use std::fs;
+
+#[tokio::test]
+async fn test_postgres_version() {
+    let container = Postgres::default().start().await.unwrap();
+    let host = container.get_host().await.unwrap();
+    let port = container.get_host_port_ipv4(5432).await.unwrap();
+
+    let conn_str = format!(
+        "host={} port={} user=postgres password=postgres dbname=postgres",
+        host, port
+    );
+
+    let (client, connection) = tokio_postgres::connect(&conn_str, NoTls).await.unwrap();
+    tokio::spawn(connection);
+
+    let row = client.query_one("SELECT version();", &[]).await.unwrap();
+    let version: &str = row.get(0);
+    println!("PostgreSQL version: {}", version);
+    assert!(version.to_lowercase().contains("postgresql"));
+}
+
+#[tokio::test]
+async fn test_postgres_connection() {
+    let container = Postgres::default().start().await.unwrap();
+    let host = container.get_host().await.unwrap();
+    let port = container.get_host_port_ipv4(5432).await.unwrap();
+
+    let conn_str = format!(
+        "host={} port={} user=postgres password=postgres dbname=postgres",
+        host, port
+    );
+
+    let (client, connection) = tokio_postgres::connect(&conn_str, NoTls).await.unwrap();
+    tokio::spawn(connection);
+
+    let row = client.query_one("SELECT 1 + 1", &[]).await.unwrap();
+    let sum: i32 = row.get(0);
+    assert_eq!(sum, 2);
+}
