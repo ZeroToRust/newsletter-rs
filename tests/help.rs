31504e071e2357f5dafc3683e8a1e744147b081f
donemmanuelo <emmanuelodon943@gmail.com>
2025-05-17 04:39:23 +0100

    added a function the return a pool

--- tests/help.rs
+++ tests/help.rs
@@ -1,19 +1,22 @@
-use testcontainers_modules::postgres::Postgres;// testcontainers::{runners::AsyncRunner, ImageExt}};
-use testcontainers_modules::{testcontainers, testcontainers::{ImageExt, runners::AsyncRunner}};
+use testcontainers_modules::postgres::Postgres; // testcontainers::{runners::AsyncRunner, ImageExt}};
+use testcontainers_modules::{
+    testcontainers,
+    testcontainers::{runners::AsyncRunner, ImageExt},
+};
 async fn start_postgres_container() -> (testcontainers::ContainerAsync<Postgres>, String) {
- let image = Postgres::default().with_tag("17.5"); // fixed tag
-        let container = image.start().await.expect("failed to launch image");
+    let image = Postgres::default().with_tag("17.5"); // fixed tag
+    let container = image.start().await.expect("failed to launch image");
 
-        let port = container
-            .get_host_port_ipv4(5432)
-            .await
-            .expect("failed to get port");
-        let conn_str = format!("postgres://postgres:postgres@localhost:{}/postgres", port);
+    let port = container
+        .get_host_port_ipv4(5432)
+        .await
+        .expect("failed to get port");
+    let conn_str = format!("postgres://postgres:postgres@localhost:{}/postgres", port);
 
     (container, conn_str)
 }
-use sqlx::{PgPool, postgres::PgPoolOptions};
 use sqlx::migrate;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 async fn setup_database_pool(database_url: &str) -> PgPool {
     let pool = PgPoolOptions::new()
@@ -28,16 +31,4 @@ async fn setup_database_pool(database_url: &str) -> PgPool {
         .expect("Failed to run migrations");
 
     pool
-}
-#[tokio::test]
-async fn test_sqlx_interacts_with_postgres_container() {
-    let (_container, connection_string) = start_postgres_container().await;
-    let pool = setup_database_pool(&connection_string).await;
-
-    let row: (i32,) = sqlx::query_as("SELECT COUNT(*) FROM  postgres")
-        .fetch_one(&pool)
-        .await
-        .expect("Query failed");
-
-    assert_eq!(row.0, 1);
 }
